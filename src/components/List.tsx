eefddddacae3f8c4f757ea05e94984d50bde13ef
Liaksej <tarusov.liaksej@gmail.com>
2023-10-16 23:40:47 +0200

    Add List and Details components, refactor page.tsx
    Introduced List and Details components and refactored page.tsx to import these new components. The List component implements user selection functionality while Details is currently empty. Both these components were created to modularize the application and improve readability and maintainability. page.tsx was refactored to replace existing boilerplate code with the List component which is now responsible for rendering user list. Also, page.tsx now initializes state for selected user and the user list, uses a reducer function to manage state updates, and triggers async user data load on component initialization.

---
+++ src/components/List.tsx
@@ -0,0 +1,33 @@
+import { SetSelectedId, User } from "@/app/page";
+import { Dispatch, MouseEvent } from "react";
+
+interface ListProps {
+  users: User[] | null;
+  selectedId: number | null;
+  dispatch: Dispatch<SetSelectedId>;
+}
+
+export const List = ({ users, dispatch, selectedId }: ListProps) => {
+  function selectHandler(event: MouseEvent<HTMLElement>, id: number) {
+    dispatch({ type: "setSelectedId", payload: id });
+    if (
+      event.currentTarget.parentNode &&
+      event.currentTarget.parentNode.childNodes
+    ) {
+      Array.from(event.currentTarget.parentNode.childNodes).forEach((node) => {
+        (node as HTMLElement).style.backgroundColor = "";
+      });
+    }
+    event.currentTarget.style.backgroundColor = "blue";
+  }
+
+  return (
+    <ul>
+      {users?.map((user) => (
+        <li key={user.id} onClick={(event) => selectHandler(event, user.id)}>
+          {user.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
